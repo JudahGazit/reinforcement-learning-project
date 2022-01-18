df4d8a11f175dd60b9672222775a5288c445d527
JudahGazit <gazitrotem@gmail.com>
2022-01-18 11:16:21 +0200

    beginning to learn, lots of noise

--- main.py
+++ main.py
@@ -1,36 +1,58 @@
 import gym
+import matplotlib
 import matplotlib.animation as animation
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import imageio
 
 from model import Model
 
-
-def play_by_model(env, model, batch_frames):
+def play_by_model(env, model, batch_frames, file_name='game.gif'):
     state = env.reset()
-    state = np.concatenate([state, np.zeros((batch_frames - 1) * len(state))])
+    state = np.hstack([state for _ in range(batch_frames)])
     frames = []
+    actions = []
+    rewards = []
     total_reward = 0
-    fig = plt.figure("Animation")
-    ax = fig.add_subplot(111)
     for i in range(500):
-        action = model.act(state.reshape(1, -1), stochastic=False)[0]
+        action = model.act(state.reshape(1, -1))[0]
         batch_states = np.array([env.step(model.action_space[action]) for _ in range(batch_frames)])
         state, reward, done, info = np.concatenate(batch_states[:, 0]), *batch_states[-1, 1:]
+        reward = np.maximum(reward, -10)
+        reward = reward if reward < 1 else (10 * reward) ** 2
         total_reward += reward
 
-        frames.append([ax.imshow(env.render(mode='rgb_array')),
-                       ax.annotate(f'{state[0]:.4f}, {reward}', (0.5, 0.9), xycoords='figure fraction')])
-
+        rewards.append(reward)
+        actions.append(action)
+        frames.append(env.render(mode='rgb_array'))
         if done:
             break
-    ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True, repeat=False)
-    return ani
+
+    imageio.mimsave(file_name, frames, duration=0.05)
+
+    plt.plot(np.arange(len(rewards)), rewards, '-o')
+    plt.title(f'reward per action - {file_name}')
+    plt.show()
+
+    plt.plot(np.arange(len(rewards)), np.cumsum(rewards), '-o')
+    plt.title(f'reward per action - cumsum - {file_name}')
+    plt.show()
+
+    counts = pd.value_counts(actions).reset_index()
+    counts['index'] = counts['index'].apply(lambda x: model.action_space[x])
+    counts.set_index('index').head(10).plot(kind='barh', ax=plt.figure(figsize=(15, 5)).gca())
+    plt.title(f'action dist - {file_name}')
+    plt.show()
+
+    # ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True, repeat=False)
+    # return ani
 
 
 if __name__ == '__main__':
     env = gym.make("BipedalWalker-v3").env
-    batch_frames = 1
-    model = Model(env, batch_frames).train()
-    animation = play_by_model(env, model, batch_frames)
-    animation.save('game.gif')
+    batch_frames = 2
+    model = Model("BipedalWalker-v3", batch_frames).train()
+    for i in range(10):
+        play_by_model(env, model, batch_frames, f'game_temp{i+1}.gif')
+    # animation.save('game.gif')
--- model.py
+++ model.py
@@ -1,37 +1,68 @@
+import itertools
+
+import gym
+import keras.optimizer_v2.adam
 import numpy as np
 import random
 import warnings
 from collections import deque
+import copy
 
 import numpy as np
-from keras.layers import Dense, Input, BatchNormalization
+import scipy.special
+from keras.layers import Dense, Input, BatchNormalization, Dropout
 from keras.models import Sequential
 
+import matplotlib.pyplot as plt
 import shelve
 
 warnings.filterwarnings("ignore")
 
+class ReplayMemory:
+    def __init__(self, max_size):
+        self.buffer = [None] * max_size
+        self.max_size = max_size
+        self.index = 0
+        self.size = 0
+        self.mean = 0
+        self.std = 1
+
+    def append(self, obj):
+        self.buffer[self.index] = obj
+        self.size = min(self.size + 1, self.max_size)
+        self.index = (self.index + 1) % self.max_size
+
+    def sample(self, batch_size):
+        indices = random.sample(range(self.size), batch_size)
+        return [self.buffer[index] for index in indices]
+
+    def __len__(self):
+        return self.size
 
 class Model:
-    def __init__(self, env, batch_frames, action_step_size=4):
-        self.env = env
+    def __init__(self, env, batch_frames, action_step_size=5):
+        self.env_name = env
+        self.env = gym.make(self.env_name).env
 
+        self.parallel_envs = 32
         self.action_step_size = action_step_size
-        self.copy_to_target_at = 32
-        self.number_of_replays = 0
-        self.minimum_states = 1000
+        self.copy_to_target_at = 500
+        self.minimum_states = 5000
         self.batch_frames = batch_frames
         self.batch_size = 32
-        self.gamma = 0.85
-        self.epsilon = 0.5
-        self.epsilon_min = 0.01
+        self.gamma = 0.95
+        self.epsilon = 0.3
+        self.epsilon_min = 0.05
         self.epsilon_decay = 0.995
+        self.learning_rate = 0.01
 
         self.action_space = self.__make_action_space()
 
-        replay_memory_pickle = shelve.open('replay.pickle', writeback=True)
-        replay_memory_pickle['mem'] = deque(maxlen=10_000)
-        self.replay_memory = replay_memory_pickle['mem']
+        # self.replay_memory_pickle = shelve.open('replay.pickle', writeback=True)
+        # if 'mem' not in self.replay_memory_pickle:
+        #     self.replay_memory_pickle['mem'] = ReplayMemory(500_000)
+        # self.replay_memory = self.replay_memory_pickle['mem']
+        self.replay_memory = ReplayMemory(500_000)
         self.model = self.create_model()
         self.target_model = self.create_model()
         self.copy_to_target()
@@ -39,11 +70,12 @@ class Model:
 
     def __make_action_space(self):
         number_of_axes = self.env.action_space.shape[0]
-        # actions = itertools.product(*[(-self.action_step_size, 0, self.action_step_size) for _ in range(number_of_axes)])
-        # actions = itertools.product(*[np.linspace(-1, 1, self.action_step_size) for _ in range(number_of_axes)])
-        # actions = np.array(list(actions))
-        actions = (np.linspace(-1, 1, self.action_step_size) * np.eye(number_of_axes)[:, :, None]).reshape(number_of_axes, -1, ).transpose()
-        actions = np.concatenate([actions, np.zeros((1, number_of_axes))])
+        # actions = (np.linspace(-1, 1, self.action_step_size) * np.eye(number_of_axes)[:, :, None]).reshape(number_of_axes, -1, ).transpose()
+        actions = itertools.product(*[(-1, 0, 1) for _ in range(number_of_axes)])
+        actions = np.array(list(actions))
+        # actions = actions[((actions == 0).sum(1) >= 2) & ((actions == 0).sum(1) < number_of_axes)]
+        actions = actions[((actions != 0).sum(1) <= 2)]
+        # actions = np.concatenate([actions, np.zeros((1, number_of_axes))])
         print('Number of actions', len(actions))
         return actions
 
@@ -52,23 +84,27 @@ class Model:
         number_of_features = self.env.observation_space.shape[0] * self.batch_frames
         print('Number of features', number_of_features)
         model.add(Input(number_of_features))
-        model.add(BatchNormalization())
-        model.add(Dense(128, activation="relu"))
-        model.add(Dense(64, activation="relu"))
-        model.add(Dense(32, activation="relu"))
+        for _ in range(2):
+            model.add(Dense(512, activation="relu"))
         model.add(Dense(len(self.action_space)))
-        model.compile(loss="mean_squared_error", optimizer='adam')
-        model.trainable = True
+        model.compile(loss="mean_squared_error", optimizer=keras.optimizer_v2.adam.Adam(clipnorm=5, learning_rate=self.learning_rate))
         return model
 
     def copy_to_target(self):
-        self.target_model.set_weights(self.model.get_weights())
+        self.target_model.set_weights(copy.deepcopy(self.model.get_weights()))
 
     def remember(self, state, action, reward, new_state, done):
         self.replay_memory.append([state, action, reward, new_state, done])
 
     def act(self, states, stochastic=True):
-        greedy_actions = np.argmax(self.model.predict(states), 1)
+        # model_actions = self.model.predict(states)
+        # try:
+        #     action_prob = scipy.special.softmax(model_actions, 1)
+        #     actions = np.array([np.random.choice(action_prob.shape[1], 1, p=p) for p in action_prob]).flatten()
+        #     return actions
+        # except:
+        #     return model_actions.argmax(1)
+        greedy_actions = np.argmax(self.model.predict((states - self.replay_memory.mean) / self.replay_memory.std, batch_size=len(states)), 1)
         actions = np.array([
                             np.random.choice(len(self.action_space)) if (np.random.random() < self.epsilon) and stochastic
                             else greedy_action
@@ -76,36 +112,62 @@ class Model:
         ])
         return actions
 
+
     def learn_over_replay(self):
-        if self.number_of_replays % self.copy_to_target_at == 0:
-            self.copy_to_target()
         if len(self.replay_memory) >= self.minimum_states:
-            self.number_of_replays += 1
-            self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
-            samples = np.array(random.sample(self.replay_memory, self.batch_size))
-            targets = self.target_model.predict(np.vstack(samples[:, 0]))
-            Q_future = self.target_model.predict(np.vstack(samples[:, 3])).max(1)
+            samples = np.array(self.replay_memory.sample(self.batch_size))
+            state = (np.vstack(samples[:, 0]) - self.replay_memory.mean) / self.replay_memory.std
+            next_state = (np.vstack(samples[:, 3]) - self.replay_memory.mean) / self.replay_memory.std
+            targets = self.model.predict(state)
+            Q_future = self.target_model.predict(next_state).max(1)
             targets[np.arange(len(targets)), samples[:, 1].astype(int)] = samples[:, 2] + (1 - samples[:, 4]) * Q_future * self.gamma
-            loss = self.model.fit(np.vstack(samples[:, 0]), targets, epochs=1, verbose=False, batch_size=self.batch_size, shuffle=False)
-            return loss
+            loss = self.model.fit(state, targets, epochs=1, verbose=False, batch_size=self.batch_size, shuffle=False)
+            return loss.history['loss'][0]
+        else:
+            self.replay_memory.mean = np.array(self.replay_memory.buffer[:self.replay_memory.size])[:, 0].mean(0)
+            self.replay_memory.std = np.array(self.replay_memory.buffer[:self.replay_memory.size])[:, 0].std(0)
+        return 0
 
-    def train(self, episodes=200, episode_length=500):
+    def train(self, episodes=500, episode_length=200):
+        envs = [gym.make(self.env_name) for _ in range(self.parallel_envs)]
+        losses = []
         for trial in range(episodes):
-            cur_state = self.env.reset()
-            cur_state = np.concatenate([cur_state, np.zeros((self.batch_frames - 1) * len(cur_state))])
+            cur_states = np.array([env.reset() for env in envs])
+            cur_states = np.hstack([cur_states for _ in range(self.batch_frames)])
             total_reward = 0
+            losses_of_trial = []
             for step in range(episode_length):
-                action = self.act(cur_state.reshape(1, -1))[0]
-                batch_states = np.array([self.env.step(self.action_space[action]) for _ in range(self.batch_frames)])
-                new_state, reward, done, _ = np.concatenate(batch_states[:, 0]), *batch_states[-1, 1:]
+                if len(cur_states) > 0:
+                    actions = self.act(cur_states)
+                    new_states = np.array([[env.step(self.action_space[action]) for _ in range(self.batch_frames)]
+                                           for env, action in zip(envs, actions)])
+                    rewards = np.maximum(new_states[:, :, 1].mean(1), -10)
+                    # rewards = rewards * (rewards < 0) + (1 * rewards * (rewards > 0)) - 0.01
+                    total_reward += rewards.mean()
+                    for state, action, reward, new_state in zip(cur_states, actions, rewards, new_states):
+                        self.remember(state, action, reward, np.hstack(new_state[:, 0]), new_state[:, 2].any())
+                    loss = self.learn_over_replay()
+                    losses_of_trial.append(loss)
 
-                reward = np.max([reward, -10])
-                total_reward += reward
-                self.remember(cur_state, action, reward, new_state, done)
-                self.learn_over_replay()
-
-                cur_state = new_state
-                if done:
-                    break
-            print('Episode', trial, 'Reward', total_reward, 'Queue', len(self.replay_memory), 'Epsilon', self.epsilon)
+                    cur_states = np.array(new_states[:, :, 0].tolist()).reshape(cur_states.shape)
+                    # cur_states = np.delete(cur_states, np.where(new_states[:, :, 2].any(1))[0], axis=0)
+                    if new_states[:, :, 2].any():
+                        break
+                # for i in np.where(new_states[:, :, 2].any(1))[0]:
+                #     reset_game = envs[i].reset()
+                #     cur_states[i] = np.hstack([reset_game for _ in range(self.batch_frames)])
+            self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+            if trial % (self.copy_to_target_at // self.parallel_envs) == 0:
+                self.copy_to_target()
+            losses.append(np.mean(losses_of_trial))
+            print('Episode', str.zfill(str(trial), 4), '\t|\t',
+                  'Reward', f'{total_reward:.4f}', '\t|\t',
+                  'Queue', len(self.replay_memory), '\t|\t',
+                  'Epsilon', f'{self.epsilon:.4f}', '\t|\t',
+                  'Loss', f'{np.mean(losses_of_trial):.4f}', '\t|\t',
+                  'TargetUpdates', trial // (self.copy_to_target_at // self.parallel_envs), '\t|\t',
+                  sep='\t')
+        plt.plot(range(episodes), losses, '-o')
+        plt.setp(plt.gca(), title='Losses per episode', xlabel='episode', ylabel='mean loss')
+        plt.show()
         return self
